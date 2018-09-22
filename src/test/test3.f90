4776853cf8887c29223a32538915ddac70cdfb1d
jvdp1 <jeremie.vandenplas@wur.nl>
2018-09-22 22:36:57 +0200

    addition of the functions to get submatrices

---
+++ src/test/test3.f90
@@ -0,0 +1,142 @@
+program test3
+ use modkind
+ use modsparse
+ implicit none
+ integer(kind=int4)::nrow
+ integer(kind=int4)::row
+ integer(kind=int4)::col
+ integer(kind=int4)::iunit, istat
+ real(kind=real8)::val
+ logical::lup=.false.
+ type(coosparse)::coo
+ type(crssparse)::crs,subcrs
+
+ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+ !COO  FULL
+ open(newunit=iunit,file='crsinput.ascii',status='old',action='read')
+ read(iunit,*) nrow
+
+ coo=coosparse(nrow,nel=4_int8,lupper=.false.)
+
+ do
+  read(iunit,*,iostat=istat) row,col,val
+  if(istat.ne.0)exit
+  call coo%add(row,col,val)
+ end do
+ close(iunit)
+
+ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+ !CSR FULL
+ crs=coo
+ call crs%printstats
+ call crs%print(lint=.true.)
+ 
+ print*,'FULL => FULL xxxxxxxxxxxxxxxxxxxxxxxxxx'
+ call crs%printsquare()
+ print*,'SELECT UPPER BLOCK xxxxxxxxxxxxxxxxxxxxxxxxxx'
+ print*,'aaaa 1 2 3 4'
+ subcrs=crs%submatrix(1,2,3,4)
+ call subcrs%printstats()
+ call subcrs%printsquare()
+
+ print*,'SELECT LOWER BLOCK xxxxxxxxxxxxxxxxxxxxxxxxxx'
+ print*,'aaaa 3 4 1 2'
+ subcrs=crs%submatrix(3,4,1,2)
+ call subcrs%printstats()
+ call subcrs%printsquare()
+
+ print*,'SELECT BLOCK + DIAG xxxxxxxxxxxxxxxxxxxxxxxxxx'
+ print*,'aaaa 1 2 1 3'
+ subcrs=crs%submatrix(1,2,1,3)
+ call subcrs%printstats()
+ call subcrs%printsquare()
+ 
+ print*,''
+ print*,'FULL => UPPER xxxxxxxxxxxxxxxxxxxxxxxxxx'
+ call crs%printsquare()
+ print*,'SELECT UPPER BLOCK xxxxxxxxxxxxxxxxxxxxxxxxxx'
+ print*,'aaaa 1 2 3 4'
+ subcrs=crs%submatrix(1,2,3,4,.true.)
+ call subcrs%printstats()
+ call subcrs%printsquare()
+
+ print*,'SELECT LOWER BLOCK xxxxxxxxxxxxxxxxxxxxxxxxxx'
+ print*,'aaaa 3 4 1 2'
+ subcrs=crs%submatrix(3,4,1,2,.true.)
+ call subcrs%printstats()
+ call subcrs%printsquare()
+
+ print*,'SELECT BLOCK + DIAG xxxxxxxxxxxxxxxxxxxxxxxxxx'
+ print*,'aaaa 1 2 1 3'
+ subcrs=crs%submatrix(1,2,1,3,.true.)
+ call subcrs%printstats()
+ call subcrs%printsquare()
+
+
+
+
+
+
+
+ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+ !COO UPPER
+ open(newunit=iunit,file='crsinput.ascii',status='old',action='read')
+ read(iunit,*) nrow
+
+ coo=coosparse(nrow,nel=4_int8,lupper=.true.)
+
+ do
+  read(iunit,*,iostat=istat) row,col,val
+  if(istat.ne.0)exit
+  call coo%add(row,col,val)
+ end do
+ close(iunit)
+
+ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+ !CSR UPPER
+ crs=coo
+ call crs%printstats()
+ call crs%print(lint=.true.)
+
+ print*,'UPPER => FULL xxxxxxxxxxxxxxxxxxxxxxxxxx'
+ call crs%printsquare()
+ print*,'SELECT UPPER BLOCK xxxxxxxxxxxxxxxxxxxxxxxxxx'
+ print*,'aaaa 1 2 3 4'
+ subcrs=crs%submatrix(1,2,3,4)
+ call subcrs%printstats()
+ call subcrs%printsquare()
+
+ print*,'SELECT LOWER BLOCK xxxxxxxxxxxxxxxxxxxxxxxxxx'
+ print*,'aaaa 3 4 1 2'
+ subcrs=crs%submatrix(3,4,1,2)
+ call subcrs%printstats()
+ call subcrs%printsquare()
+
+ print*,'SELECT BLOCK + DIAG xxxxxxxxxxxxxxxxxxxxxxxxxx'
+ print*,'aaaa 1 2 1 3'
+ subcrs=crs%submatrix(1,2,1,3)
+ call subcrs%printstats()
+ call subcrs%printsquare()
+ 
+ print*,''
+ print*,'UPPER => UPPER xxxxxxxxxxxxxxxxxxxxxxxxxx'
+ call crs%printsquare()
+ print*,'SELECT UPPER BLOCK xxxxxxxxxxxxxxxxxxxxxxxxxx'
+ print*,'aaaa 1 2 3 4'
+ subcrs=crs%submatrix(1,2,3,4,.true.)
+ call subcrs%printstats()
+ call subcrs%printsquare()
+
+ print*,'SELECT LOWER BLOCK xxxxxxxxxxxxxxxxxxxxxxxxxx'
+ print*,'aaaa 3 4 1 2'
+ subcrs=crs%submatrix(3,4,1,2,.true.)
+ call subcrs%printstats()
+ call subcrs%printsquare()
+
+ print*,'SELECT BLOCK + DIAG xxxxxxxxxxxxxxxxxxxxxxxxxx'
+ print*,'aaaa 1 2 1 3'
+ subcrs=crs%submatrix(1,2,1,3,.true.)
+ call subcrs%printstats()
+ call subcrs%printsquare()
+
+end program
