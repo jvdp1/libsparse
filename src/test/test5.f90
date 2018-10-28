3a8ec3e1208502374c12706f723f59e51f5b3d06
jvdp1 <jeremie.vandenplas@wur.nl>
2018-10-28 21:13:31 +0100

    small changes

--- src/test/test5.f90
+++ src/test/test5.f90
@@ -10,8 +10,11 @@ program test5
  integer(kind=int32)::row
  integer(kind=int32)::col
  integer(kind=int32)::iunit, istat
- integer(kind=int32),allocatable::iperm(:)
+ integer(kind=int32)::i,j
+ integer(kind=int32),allocatable::iarray(:,:)
+ integer(kind=int32),allocatable::perm(:)
  real(kind=wp)::val
+ real(kind=wp),allocatable::x(:),y(:)
  logical::lup=.false.
  type(coosparse)::coo
  type(crssparse)::crs
@@ -33,14 +36,37 @@ program test5
  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
  !CSR UPPER
  crs=coo
- call crs%printstats()
  call crs%print(lint=.true.)
 
- iperm=crs%getordering()
+ perm=crs%getordering()
+ print*,'perm',perm
  
- print*,'iperm',iperm
- 
+ call crs%printstats()
 
 
+ write(*,*)'aaaaaaaaaaaa'
+
+ call crs%setpermutation(crs%getordering())
+ call crs%printstats()
+
+ allocate(x(crs%getdim(1)),y(crs%getdim(1)))
+ x=1.;y=1.
+
+ call crs%solve(x,y)
+
+
+ write(*,*)'aaaaaaaaaaaa'
+ allocate(iarray(crs%getdim(1),crs%getdim(2)))
+ iarray=0
+ do i=1,crs%getdim(1)
+  do j=1,crs%getdim(2)
+   val=crs%get(perm(i),perm(j))
+   if(val.ne.0.)iarray(i,j)=1
+  enddo
+ enddo
+
+ do i=1,crs%getdim(1)
+  write(*,'(10000(i2))')iarray(i,:)
+ enddo
 
 end program
